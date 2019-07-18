4e43096c5dc2cef231f0c40901e2241bd6b9e17b
Connor Perez <connorperez@gmail.com>
2019-07-18 09:52:29 -0500

    creating note and save functions

---
+++ controllers/notes.js
@@ -0,0 +1,30 @@
+var Note = require("../models/note");
+var makeDate = require("../scripts/date");
+
+module.exports = {
+
+    get: function(data, cb){
+        Note.find({
+            headlineId: data._id
+        }, cb);
+    },
+
+    save: function(data, cb){
+        var newNote = {
+            _headlineId: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+        };
+
+        Note.create(newNote, function (err, doc){
+            if (err){
+                console.log(err);
+            }
+            else {
+                console.log(doc);
+                cb(doc);
+            }
+        });
+    },
+
+}
